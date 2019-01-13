d93fad71392a0bd208a2ba3976768285f05e7e82
Dhondi Srikanth <dhondi.srikanth@gmail.com>
2019-01-13 22:38:52 +0530

    Single Page Applications Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'about',  component: AboutComponent },
   { path: 'menu',     component: MenuComponent },
-  { path: 'contact',  component: ContactComponent },
+  { path: 'contactus',  component: ContactComponent },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
